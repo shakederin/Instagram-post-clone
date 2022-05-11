74551f4383a87287110f40948896700e1bf3517b
Shaked Erinberg <shakede@wix.com>
2022-05-11 17:08:42 +0300

    adding coment and like comment

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,16 @@ const inputElement = document.getElementById("commentInput") as HTMLInputElement
 const formElement = document.getElementById("inputForm");
 const ulElement = document.getElementById("commentsList") as HTMLUListElement;
 const postElement = document.getElementById("postComment");
+
+//post comment button
 postElement?.addEventListener("mouseenter", ()=>{
     postElement.style.color = "#3FA7DB";    
 })
 postElement?.addEventListener("mouseleave", ()=>{
     postElement.style.color = "#A5D4EB";    
 })
-console.log(122);
 
-console.log(formElement);
-console.log(inputElement);
-console.log(ulElement);
-console.log(postElement);
-
+//submit form
 if (formElement !== null) {
     console.log("im here");
     formElement?.addEventListener("submit", (event) => {
@@ -47,7 +44,7 @@ if (formElement !== null) {
             newhourAgoElement.innerText = "10 h";
             const newlikesInfoElement = document.createElement("b");
             newlikesInfoElement.className = "infoSubtitle";
-            newlikesInfoElement.innerText = "1 like";
+            newlikesInfoElement.innerText = "like";
             const newReplayInfoElement = document.createElement("b")
             newReplayInfoElement.className = "infoSubtitle";
             newReplayInfoElement.innerText = "Reply";
@@ -55,8 +52,19 @@ if (formElement !== null) {
             // heart in the end
             const newCommentSvgElement = document.createElement("img");
             newCommentSvgElement.classList.add("svgImg", "commentHeart");
-            newCommentSvgElement.src = "../public/assets/heart-3511.svg";
+            newCommentSvgElement.src = "../public/assets/heart-3511.svg"
+            
+            newCommentSvgElement.style.fontSize = "0.55em"
             newCommentSvgElement.alt = "heart";
+            newCommentSvgElement.addEventListener("click", ()=>{
+                if(newlikesInfoElement.innerText === "like"){
+                    newlikesInfoElement.innerText = "1 like"
+                    newCommentSvgElement.src = "../public/assets/redHeart.svg"
+                }else{
+                    newlikesInfoElement.innerText = "like"
+                    newCommentSvgElement.src = "../public/assets/heart-3511.svg";
+                }
+            })
             // li father
             newLiElement.className = "comment";
             newLiElement.append(newImgElement, newCommentHeaderElement, newCommentInfoElement, newCommentSvgElement);
@@ -66,20 +74,5 @@ if (formElement !== null) {
     })
 }
 
-console.log(2);
 
 
-// <li class="comment">
-//     <img src="https://cdn.pixabay.com/photo/2021/05/20/11/58/beauty-6268460_1280.jpg"
-//         alt="profile Picture" class="profilePicture">
-//     <div class="commentHeader">
-//         <b>tonytoscani</b>
-//         &#8226; Following
-//     </div>
-//     <div class="commentInfo">
-//         <div class="infoSubtitle">10 h</div>
-//         <div class="infoSubtitle"><b>1 like</b></div>
-//         <div class="infoSubtitle"><b>Reply</b></div>
-//     </div>
-//     <img class="svgImg commentHeart" src="assets/heart-3511.svg" alt="heart">
-// </li>
