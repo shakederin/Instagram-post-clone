92fb94084498707e299df7b2cefe8f7f0ac395c9
Shaked Erinberg <shakede@wix.com>
2022-05-12 15:35:42 +0300

    starting to fix Css (pic, header, UL)

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-const inputElement = document.getElementById("commentInput") as HTMLInputElement;
+const inputElement = document.getElementById("commentInput");
 const formElement = document.getElementById("inputForm");
-const ulElement = document.getElementById("commentsList") as HTMLUListElement;
+const ulElement = document.getElementById("commentsList");
 const postElement = document.getElementById("postComment");
 
 //post comment button
-postElement?.addEventListener("mouseenter", ()=>{
-    postElement.style.color = "#3FA7DB";    
+postElement?.addEventListener("mouseenter", () => {
+    postElement.style.color = "#3FA7DB";
 })
-postElement?.addEventListener("mouseleave", ()=>{
-    postElement.style.color = "#A5D4EB";    
+postElement?.addEventListener("mouseleave", () => {
+    postElement.style.color = "#A5D4EB";
 })
 
 //submit form
 if (formElement !== null) {
-    console.log("im here");
     formElement?.addEventListener("submit", (event) => {
         event.preventDefault();
-        if (inputElement !== null ) {
-            const newMsg = inputElement.value;
-            if(newMsg === ""){
+        if (inputElement !== null) {
+            const newMsg = (inputElement as HTMLInputElement).value;
+            if (newMsg === "") {
                 return;
             }
             // li father
@@ -29,13 +28,18 @@ if (formElement !== null) {
             newImgElement.src = "https://cdn.pixabay.com/photo/2021/05/20/11/58/beauty-6268460_1280.jpg";
             newImgElement.alt = "profile Picture";
             // comment 
+            const newCommentElement = document.createElement("div");
+            newCommentElement.className = "comonComment";
+            //comment Header
             const newCommentHeaderElement = document.createElement("div");
+            const newCommentHeaderAndContentElement = document.createElement("div");
             const newCommentBodyElement = document.createElement("span");
             newCommentHeaderElement.className = "commentHeader";
             newCommentBodyElement.innerText = ` ${newMsg}`;
             const newCommentHeaderBoldElement = document.createElement("b");
             newCommentHeaderBoldElement.innerText = "tonytoscani";
-            newCommentHeaderElement.append(newCommentHeaderBoldElement, newCommentBodyElement);
+            newCommentHeaderAndContentElement.append(newCommentHeaderBoldElement, newCommentBodyElement);
+            newCommentHeaderElement.append(newCommentHeaderAndContentElement);
             // extra info
             const newCommentInfoElement = document.createElement("div");
             newCommentInfoElement.className = "commentInfo";
@@ -49,27 +53,32 @@ if (formElement !== null) {
             newReplayInfoElement.className = "infoSubtitle";
             newReplayInfoElement.innerText = "Reply";
             newCommentInfoElement.append(newhourAgoElement, newlikesInfoElement, newReplayInfoElement);
+            newCommentHeaderElement.append(newCommentInfoElement)
             // heart in the end
             const newCommentSvgElement = document.createElement("img");
-            newCommentSvgElement.classList.add("svgImg", "commentHeart");
+            newCommentSvgElement.className = "commentHeart";
             newCommentSvgElement.src = "../public/assets/heart-3511.svg"
-            
+
             newCommentSvgElement.style.fontSize = "0.55em"
             newCommentSvgElement.alt = "heart";
-            newCommentSvgElement.addEventListener("click", ()=>{
-                if(newlikesInfoElement.innerText === "like"){
+            newCommentSvgElement.addEventListener("click", () => {
+                if (newlikesInfoElement.innerText === "like") {
                     newlikesInfoElement.innerText = "1 like"
                     newCommentSvgElement.src = "../public/assets/redHeart.svg"
-                }else{
+                } else {
                     newlikesInfoElement.innerText = "like"
                     newCommentSvgElement.src = "../public/assets/heart-3511.svg";
                 }
             })
             // li father
             newLiElement.className = "comment";
-            newLiElement.append(newImgElement, newCommentHeaderElement, newCommentInfoElement, newCommentSvgElement);
+            newCommentElement.append(newImgElement, newCommentHeaderElement);
+            newLiElement.append(newCommentElement, newCommentSvgElement);
+            if (ulElement === null) {
+                return
+            }
             ulElement.append(newLiElement);
-            inputElement.value = "";
+            (inputElement as HTMLInputElement).value = "";
         }
     })
 }
