ccffdb4e9138062063e13d8071c00973094a9c84
Shaked Erinberg <shakede@wix.com>
2022-05-23 12:00:49 +0300

    rename and refactor Yarden review

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import { makeComment } from "./utils/createElements.js";
+import { createComment } from "./utils/createComment.js";
+
 const inputElement = document.getElementById("commentInput");
 const formElement = document.getElementById("inputForm");
 const ulElement = document.getElementById("commentsList");
-const ppURL =
+const profilePicture =
   "https://cdn.pixabay.com/photo/2021/05/20/11/58/beauty-6268460_1280.jpg";
 
-if (
-  formElement instanceof HTMLFormElement &&
-  inputElement instanceof HTMLInputElement
-) {
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const newMsg = inputElement.value;
-    if (newMsg && ulElement) {
-      ulElement.append(makeComment(ppURL, "tonytoscani", newMsg));
-      inputElement.value = "";
-    }
-  });
-}
+window.addEventListener("load", () => {
+  if (
+    formElement instanceof HTMLFormElement &&
+    inputElement instanceof HTMLInputElement
+  ) {
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const newComment = inputElement.value;
+      if (newComment && ulElement) {
+        ulElement.append(
+          createComment(profilePicture, "tonytoscani", newComment)
+        );
+        formElement.reset();
+      }
+    });
+  }
+});
